3adf8f8c442463d2e913b242a461003d9aa05eb0
Juangui <juan-ramirez04@hotmail.com>
2024-03-21 16:04:27 -0500

    solution homework 04 http and express

--- clase-2/04.express.js
+++ clase-2/04.express.js
@@ -6,3 +6,30 @@
   1. Crear un endpoint que permita obtener la lista completa de elementos de una lista de mercado utilizando Node.js HTTP.
   2. Crear un endpoint que permita agregar un nuevo elemento a una lista de mercado utilizando Node.js HTTP, validando si el elemento ya existe y devolviendo un mensaje de error con código de estado de conflicto en caso afirmativo.
  */
+
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+let marketList = [];
+
+app.use(express.json());
+
+app.get("/marketList", (req, res) => {
+  res.status(200).json({"Market List" : marketList});
+});
+
+app.post("/marketList", (req, res) => {
+  const newItem = req.body.newItem.toLowerCase();
+
+  if (marketList.map(item => item.toLowerCase()).includes(newItem)) {
+    res.status(409).json({ message: `The item ${newItem} already exists in the list`, marketList });
+  } else {
+    marketList.push(newItem);
+    res.status(200).json({ message: `${newItem} added to the list` });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT} 🚀`);
+});
--- clase-2/04.http.js
+++ clase-2/04.http.js
@@ -6,3 +6,48 @@
   1. Crear un endpoint que permita obtener la lista completa de elementos de una lista de mercado utilizando Node.js HTTP.
   2. Crear un endpoint que permita agregar un nuevo elemento a una lista de mercado utilizando Node.js HTTP, validando si el elemento ya existe y devolviendo un mensaje de error con código de estado de conflicto en caso afirmativo.
  */
+
+import http from "http";
+
+const PORT = 3000;
+
+let marketList = [];
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/marketList") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify(marketList));
+  }
+
+  if (req.method === "POST" && req.url === "/marketList") {
+    let reqBody = "";
+
+    req.on("data", (chunk) => {
+      reqBody += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const newItem = JSON.parse(reqBody).newItem;
+      if (marketList.includes(newItem)) {
+        res.writeHead(409, {
+          "Content-Type": "application/json",
+        });
+        res.end(
+          JSON.stringify({ message: "The item already exists in the list" })
+        );
+      } else {
+        marketList.push(newItem);
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Item added to the list" }));
+      }
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Node HTTP server running at http://localhost:${PORT}`);
+});
